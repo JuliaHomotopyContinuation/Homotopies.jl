049c856a97f332381c68b13e0b0da14c910ddfd2
Sascha Timme <sascha.timme@gmail.com>
2017-11-21 15:32:41 +0100

    Comment

--- src/bigfloat_utilities.jl
+++ src/bigfloat_utilities.jl
@@ -1,7 +1,3 @@
-"""
-Simulates svd for bigfloat matrices by converting to Float64/Complex128.
-"""
-
-
+#Simulates svd for bigfloat matrices by converting to Float64/Complex128.
 Base.LinAlg.svdfact!(A::Matrix{BigFloat}; thin=true) = svdfact!(convert.(Float64, A); thin=true)
 Base.LinAlg.svdfact!(A::Array{Complex{BigFloat},2}; thin=true) = svdfact!(convert.(Complex128, A); thin=true)
